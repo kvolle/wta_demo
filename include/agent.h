d6fdaec7b5bef96dba5473718eac63ff7c985512
Kyle Volle <kvolle3@gatech.edu>
2018-06-01 10:42:33 -0500

    Initial commit. Documentation is in need of much work and the code is non-functional

---
+++ include/agent.h
@@ -0,0 +1,86 @@
+#ifndef INCLUDE_AGENT_H
+#define INCLUDE_AGENT_H
+
+#include <geometry_msgs/TransformStamped.h>
+#include "kobuki_msgs/RobotStateEvent.h"
+#include "string"
+#include "ros/ros.h"
+#include "ros/subscriber.h"
+#include "ros/publisher.h"
+#include "wta_demo/StateMsg.h"
+
+/**
+* @struct Location
+* @details Structure for holding (x,y) of object as floats
+*/
+struct Location {
+    float x_pos; /**< Holds the x-axis value of the position */
+    float y_pos; /**< Holds the y-axis value of the position */
+};
+/**
+ * @brief Target struct
+ * @detials Structure for holding information pertaining to a given target
+ */
+struct Target {
+    std::string topic;
+    float desired_pk=0.0;
+    Target(std::string t, float pk):topic(t), desired_pk(pk){}
+};
+
+/**
+* @struct Model
+* @details Used for message passing.  ADD MORE KYLE.
+*/
+struct Model {
+        Location position;  /**< TBD */
+        int target_id;  /**< TBD */
+        int confidence_index;  /**< TBD */
+        float attrition_estimate;  /**< TBD */
+        bool ready;  /**< TBD */
+        int heartbeats;  /**< TBD */
+        Model():ready(false),target_id(0),heartbeats(0){}  /**< TBD */
+        //ros::Subscriber state_sub;
+};
+class Agent {
+    public:
+        // The following are stolen from Env_Obj
+        ros::NodeHandle m_n;
+        Location location_marker;
+        ros::Subscriber StateSubscriber;
+        int id;
+
+
+        bool ready; /**< Represents if the bot is ready to move to target or not */
+        int num_targets; /**< The number of targets in the environment */
+        int num_agents; /**< The number of agents in the environment */
+        std::vector<Target> all_targets; /**< A vector holding all of the target's in the environment */
+        std::vector<float> pk_on_targets; /**< A vector holding all of the target Pk values */
+        wta_demo::StateMsg msg; /**< A message that holds the x and y position of the bot*/
+        std::vector<Model> models; /**< TBD - KYLE */
+        std::vector<ros::Subscriber> state_msg_subscriptions; /**< A vector of subscribers to the other turtlebots in the environment */
+        float max_cost; /**< TBD - KYLE */
+
+        ros::Subscriber ownPositionSubscriber; /**< TBD - KYLE */
+        ros::Subscriber hardwareSubscriber; /**< TBD - KYLE */
+        ros::Publisher publisher; /**< Publishes this agent's state message */
+        Location actual_goal; /**< The location of the agent's selected target */
+        ros::Publisher desired_state; /**< The position of the agent's desired target */
+        geometry_msgs::TransformStamped desired_state_msg; /**< TBD - KYLE */
+        std::vector<float> effectiveness; /**< TBD - KYLE */
+
+        Agent(ros::NodeHandle& n, ros::NodeHandle& nPrivate, int id,int target_number,int agent_number);  /**<Constructor of Agent class */
+        ~Agent();  /**< Destructor of Agent class */
+        void ownPositionCallback(const geometry_msgs::TransformStamped::ConstPtr& ); /**< Updates bot's location and location value inside Model */
+        void stateCallback(const wta_demo::StateMsg::ConstPtr&); /**< Stores turtlbot's position while updating target, attrition value, heartbeat counter, and bot's ready state */
+        void hardwareStateCallback(const kobuki_msgs::RobotStateEvent::ConstPtr&); /**< Sets bot to ready when turtlebot base moves into state 3 */
+        void subscribeToTopic(ros::Subscriber, std::string); /**< Lets bots subscribe to other bots running in the environment */
+        void broadcast(); /**< Broadcasts a message to all other agents */
+        void pk_from_model(); /**< Creates an estimate of Pk against all targets */
+        void decision_function(); /**< KYLE FIX */
+        void simulated_annealing(); /**< Used for agent selection of target because it examines what happens if the bot changes targets */
+        float get_distance(Location , Location ); /**< Takes the position of two items and calculates distance.  Still needed? */
+        float attrition_estimate(int); /**< Estimates probability of attrition */
+        float cost_function(std::vector<float> &); /**< Simple cost function KYLE FIX */
+        float cost_function_tiers(std::vector<float> &); /** Cost function using tiers (hardcoded) */
+};
+#endif  // INCLUDE_AGENT_H
---
+++ src/agent_node.cpp
@@ -0,0 +1,122 @@
+#include "geometry_msgs/TransformStamped.h"
+#include "ros/ros.h"
+
+//#include "../include/bot_communicator.h"
+#include "../include/agent.h"
+//#include "../include/target.h"
+
+#include <cmath>
+#include <sstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <typeinfo>
+#include <vector>
+
+/**
+*************************************************************************************************************
+* @brief This file contains the logic needed for a turtlebot to choose its goal based on other turtlebot
+* locations.
+*
+* Project:  Pathfinder2
+* @file agent_node.cpp
+*
+* @author Brian Janisch bjanisch@tamu.edu
+* @author Allison Holt allison.holt@auburn.edu
+* @author Kyle Volle kyle.volle@gmail.com
+* @date 08/07/2015
+*
+* @details Agent node allows a turtlebot to analyze its position, the position of the other turtlebots,
+* and the Pk and attrition of the goals in order to determine which goal each turtlebot should move towards.
+* As of now, the goals chosen are based on the maximum Pk.
+*************************************************************************************************************
+*/
+
+/**
+* Author:  Brian Janisch
+* @details The main function grabs the parameters from the launch file (turtlebot name, number of turtlebots, and
+* number of goals) and stores them in an agent object.  Using that information and the information given
+* from all the subscribers, the main method calculates the different options for the Pk and attrition values between
+* the turtlebots and the goals.  The main method will select the target with the highest probability of success.  The
+* turtlebots are constantly reevaluating their choice, so, if something in the environment changes, then the turtlebots
+* will adapt.
+*
+* @param [in] argc The number of arguments passed in from the command line
+* @param [in] argv An vector containing the arguments passed in froctum the command line
+*/
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "goal_selector");
+    ros::NodeHandle n;
+    ros::NodeHandle n_private("~");
+    std::string BOT_ID;
+    int NUM_GOALS, NUM_BOTS;
+
+    geometry_msgs::TransformStamped msg; /** The message type to publish desired_state after cost function evalulation */
+
+    /* Get params from launch file */
+
+    if (!n_private.getParam("bot_id", BOT_ID))
+    {
+        //std::cout <<"Couldn't get bot_id, setting default..." <<std::endl;
+        BOT_ID = "bot0";
+    }
+
+    if (!n_private.getParam("num_goals", NUM_GOALS))
+    {
+        std::cout <<"Couldn't get num_goals, setting default..." <<std::endl;
+        NUM_GOALS = 2;
+    }
+
+    if (!n_private.getParam("num_bots", NUM_BOTS))
+    {
+        std::cout <<"Couldn't get num_bots, setting default..." <<std::endl;
+        NUM_BOTS= 2;
+    }
+
+    std::string tmp = BOT_ID.substr(BOT_ID.length()-1,1);
+    Agent agent(n,n_private,atoi(tmp.c_str()),NUM_GOALS,NUM_BOTS);
+    std::cout << "Agent created\n";
+    ros::Rate loop_rate(10);
+    srand(time(NULL));
+    while (ros::ok())
+    {
+
+        agent.broadcast();
+        std::cout<<"ready"<<agent.ready<<std::endl;
+        //if (agent.ready)
+        //{
+            // This rand() function is to prevent a race condition resulting in agents alternating quickly
+            //if (rand()%100<50) // commented June 09, 2016
+            //{
+                agent.decision_function();
+               // agent.simulated_annealing();
+            //}
+        //printf("xpos: %4.3f\nypos: %4.3f\ntarget: %d\n",agent.models[agent.id].position.x_pos,agent.models[agent.id].position.y_pos,agent.models[agent.id].target_id);
+        //}
+
+        // If another agent hasn't been heard from in 6 cycles, assume it is dead
+        for (uint a=0;a<NUM_BOTS;a++)
+        {
+          //std::cout << "BOT 0 <3: " << agent.models[0].heartbeats << "  BOT 1 <3: " << agent.models[1].heartbeats << "\n";
+
+          if ((a!=agent.id)&&(agent.models[a].ready==1)&&(agent.models[a].heartbeats >10))
+          {
+            // Attrited agent unable to reach its target, this alone should be enough
+            agent.models[a].attrition_estimate = 1.0;
+            // Setting the desired target to null should also be enough to accomplish the goal
+            agent.models[a].target_id = 0;
+            agent.models[a].ready = true;
+            //std::cout << "Agent " << a<< " is assumed to be targeting 0*****\n";
+          }
+
+          if (a!=agent.id)
+          {
+            agent.models[a].heartbeats++;
+          }
+        }
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+    return 0;
+}
