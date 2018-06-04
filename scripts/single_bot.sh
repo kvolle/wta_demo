bb4714a22c92554425d7c014c3590464b7a43ccc
Kyle Volle <kvolle3@gatech.edu>
2018-06-04 17:24:52 -0500

    This appears to be the correct bringup. Need to make sure it exists on the turtlebots

--- scripts/single_bot.sh
+++ scripts/single_bot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Launch 1 turtlebot
-roslaunch turtlebot_bringup minimal.launch &
+roslaunch turtlebot_bringup pid_gps.launch name:=/$1&
 roslaunch bot_pid_kobuki bot_pid_kobuki_min.launch name:=/$1 &
 roslaunch wta_demo wta_demo.launch name:=/$1 &
 
