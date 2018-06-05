de44366519f12973ece5dd464019da4ee52fa42e
Kyle Volle <kvolle3@gatech.edu>
2018-06-05 11:14:35 -0500

    Changed launch files

--- scripts/single_bot.sh
+++ scripts/single_bot.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Launch 1 turtlebot
-roslaunch turtlebot_bringup pid_gps.launch name:=/$1&
-roslaunch bot_pid_kobuki bot_pid_kobuki_min.launch name:=/$1 &
+roslaunch wta_demo turtlebot.launch name:=/$1&
+#roslaunch bot_pid_kobuki bot_pid_kobuki_min.launch name:=/$1 &
 roslaunch wta_demo wta_demo.launch name:=/$1 &
 
 
