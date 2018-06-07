a083fb17e4cb872fca6612141bae02d74850e795
Kyle Volle <kvolle3@gatech.edu>
2018-06-07 12:02:08 -0500

    Fixed virtual target names

--- scripts/master.sh
+++ scripts/master.sh
@@ -8,8 +8,8 @@ roscore &
 sleep 2
 
 # Launch 2 virtual targets
-roslaunch virtual_target virtual_target.launch ns:=goal0 x:=-1.0 y:=-2.0 &
-roslaunch virtual_target virtual_target.launch ns:=goal1 x:=1.0 y:=2.0 &
+roslaunch virtual_target virtual_target.launch ns:=goal1 x:=-1.0 y:=-2.0 &
+roslaunch virtual_target virtual_target.launch ns:=goal2 x:=1.0 y:=2.0 &
 
 # Wait and clean up on exit
 read -p "Press enter to exit"
