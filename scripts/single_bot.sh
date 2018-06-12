8e5f8af22114bfcb10ef904dec1f757b3a413f21
Kyle Volle <kvolle3@gatech.edu>
2018-06-12 12:51:50 -0500

    Removed scripts that are no longer needed. Scripts folder moved to quarantine

--- scripts/single_bot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Launch 1 turtlebot
-roslaunch wta_demo turtlebot.launch name:=/$1&
-#roslaunch bot_pid_kobuki bot_pid_kobuki_min.launch name:=/$1 &
-roslaunch wta_demo wta_demo.launch name:=/$1 &
-
-
